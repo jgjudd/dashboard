09d91acf3e149736e96566f65b615944c550d4bc
Justin <jjudd10@ford.com>
2020-06-05 23:15:29 -0400

    react and app setup

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <header>Header</header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </div>
+  )
+}
+
+export default App
